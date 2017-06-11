4367daedab6fcc9b3778d4a57b32e2d722343e79
Jack Ford <jford@salsify.com>
2017-06-11 10:00:34 -0400

    Fix user has_many with conversation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ApplicationRecord
-  has_many :conversations
+  has_many :sender_conversations,
+    foreign_key: :sender_id,
+    dependent: :destroy
+  has_many :receiver_conversations,
+    foreign_key: :receiver_id,
+    dependent: :destroy
+
+  # This it the main conversation-related API
+  # Sender and receiver are really just implementation details
+  def conversations
+    sender_conversations + receiver_conversations
+  end
 end
